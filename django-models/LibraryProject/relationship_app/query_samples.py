90bee6ef881bf8974d45ede0cae41d0b235be8e2
muimi04 <matthewmusili2@gmail.com>
2025-07-19 15:34:13 +0300

    Add relationship_app with complex models and sample queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,41 @@
+import os
+import django
+
+# Set up Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
+django.setup()
+
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
-books_by_author = Book.objects.filter(author=author)
-print("Books by George Orwell:", list(books_by_author))
+# 1. Query all books by a specific author
+print("\n--- Books by a specific author ---")
+author = Author.objects.first()
+if author:
+    books_by_author = Book.objects.filter(author=author)
+    print(f"Books by {author.name}:")
+    for book in books_by_author:
+        print("-", book.title)
+else:
+    print("No authors found.")
 
-# List all books in a library
-library = Library.objects.get(name="City Library")
-books_in_library = library.books.all()
-print("Books in City Library:", list(books_in_library))
+# 2. List all books in a library
+print("\n--- Books in a library ---")
+library = Library.objects.first()
+if library:
+    books_in_library = library.books.all()
+    print(f"Books in {library.name}:")
+    for book in books_in_library:
+        print("-", book.title)
+else:
+    print("No libraries found.")
 
-# Retrieve the librarian for a library
-library = Library.objects.get(name="City Library")
-librarian = Librarian.objects.get(library=library)
-print("Librarian of City Library:", librarian.name)
+# 3. Retrieve the librarian for a library
+print("\n--- Librarian for a library ---")
+if library:
+    try:
+        librarian = Librarian.objects.get(library=library)
+        print(f"Librarian for {library.name} is {librarian.name}")
+    except Librarian.DoesNotExist:
+        print("No librarian assigned to this library.")
+else:
+    print("No libraries available to retrieve librarian.")
