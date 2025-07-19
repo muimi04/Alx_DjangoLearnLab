73325f3cc686ec1a1c4a3612fc2afe0a54158b6c
muimi04 <matthewmusili2@gmail.com>
2025-07-19 16:46:15 +0300

    Add Library and Book models with ManyToMany relationship and views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
-from .models import Book, Library
-from django.views.generic.detail import DetailView
+# relationship_app/views.py
 
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-based view
 def list_books(request):
     books = Book.objects.select_related('author').all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
