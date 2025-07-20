462e8c0df82eee7a93900e3947f50455efdb3b03
muimi04 <matthewmusili2@gmail.com>
2025-07-20 14:43:43 +0300

    Add views and templates for listing books and library details

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# LibraryProject/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('', include('relationship_app.urls')),  # ✅ this is critical
 ]
+
+
--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,28 +1,15 @@
+# relationship_app/models.py
+
 from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+class Library(models.Model):
+    name = models.CharField(max_length=100)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
