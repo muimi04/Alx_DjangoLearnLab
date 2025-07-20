cbdeeda790820df07c7c1476be280c190f0702fa
muimi04 <matthewmusili2@gmail.com>
2025-07-20 12:52:06 +0300

    Add views, URLs, and templates for listing books and library detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,8 +9,9 @@ def list_books(request):
     books = Book.objects.select_related('author').all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
