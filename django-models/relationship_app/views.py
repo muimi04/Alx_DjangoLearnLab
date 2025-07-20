37c8f5a4848fc70d0e5bc9b6517831c3ec976d50
muimi04 <matthewmusili2@gmail.com>
2025-07-20 13:11:46 +0300

    Fix: moved manage.py and app to root level for ALX checker

---
+++ django-models/relationship_app/views.py
@@ -0,0 +1,17 @@
+# relationship_app/views.py
+
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-based view
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
