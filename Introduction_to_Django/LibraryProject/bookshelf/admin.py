15f333f07ec038fd36bed63701c84ee25a7b9adb
muimi04 <matthewmusili2@gmail.com>
2025-07-21 17:34:57 +0300

    Add Book model, admin integration, and CRUD documentation

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+# Customize the admin display
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to show in admin list
+    list_filter = ('publication_year', 'author')            # Add sidebar filters
+    search_fields = ('title', 'author')                     # Enable search functionality
+
 
