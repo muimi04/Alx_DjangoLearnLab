792688fb79e61bdcf22f339e60875e25ab2b9967
muimi04 <matthewmusili2@gmail.com>
2025-07-22 18:24:54 +0300

    Fix views and templates for book list and library detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from .models import Book
-
-def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
- 
-
- from django.views.generic.detail import DetailView
+from django.views.generic import DetailView
 from .models import Library
 
+# Function-based view: List all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-based view: Library detail
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
+
 
