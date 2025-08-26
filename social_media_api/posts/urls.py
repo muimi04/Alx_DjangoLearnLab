e05a859c463d6302de436baf5d291dd04f8d3f8a
muimi04 <matthewmusili2@gmail.com>
2025-08-26 19:10:46 +0300

    Added like and unlike URL patterns alongside router URLs in posts/urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,16 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView
 from django.urls import path, include  
 import posts.views as views
-
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
-urlpatterns = router.urls
-
 urlpatterns = [
     path("", include(router.urls)),
     path('feed/', views.feed, name='feed'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
