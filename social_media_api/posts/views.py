835e429196adefb3c8f47df7452c1b4c65cb2af4
muimi04 <matthewmusili2@gmail.com>
2025-08-26 19:04:37 +0300

    Updated Like and Unlike views to use generics.get_object_or_404 as required

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -66,7 +66,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
@@ -88,7 +88,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         try:
             like = Like.objects.get(user=request.user, post=post)
             like.delete()
