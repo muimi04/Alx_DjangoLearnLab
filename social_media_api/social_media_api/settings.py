27ded6596bc89d5cb93ae03b3217f94d4ec289ae
muimi04 <matthewmusili2@gmail.com>
2025-08-30 14:38:51 +0300

    Update settings for database and port configurations

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -100,6 +100,8 @@ if os.getenv("USE_S3") == "True":
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    PORT = os.getenv("PORT", 5432)  # Defaults to 5432 if not set in .env
+
 
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
